b025a87bc265367518aa100e3b5b760f357f85e3
Dave Banwell <dave@davebanwell.com>
2024-06-24 00:03:10 -0400

    updated filters and buttons

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ document.addEventListener('DOMContentLoaded', main);
 
 function main() {
 
+	// page view modes and filter functions
+
+	const viewMode = {
+		active: (entry) => !entry.isArch,
+		archive: (entry) => entry.isArch,
+		accent: (entry) => entry.isAccent
+	}
+
 	// load all entries
 
-	loadEntries();
+	const jrnlDataPath = 'http://localhost:3000/journalEntries';
 
-	// Object classes
+	loadEntries(viewMode.active);
+
+	// object classes
 
 	class jrnlObj {
 		constructor(title, content) {
@@ -68,16 +78,16 @@ function main() {
 			this.archBtn = document.createElement('div');
 			this.archBtn.id = `entry_archive_btn_${entryObj.id}`;
 			this.archBtn.className = 'entry_archive_btn';
-			this.archBtn.title = !entryObj.isArch ? 'Archive this entry' : 'Restore this entry to the journal';
+			this.archBtn.title = !entryObj.isArch ? 'Archive this entry' : 'Restore this entry';
 			this.archBtn.addEventListener('click', () => {
 				const updArr = [['isArch', !entryObj.isArch]];
 				if (!entryObj.isArch) updArr.push(['isAccent', false]);
 				pushUpdate(new updateObj(this.entry.id, updArr));
 				const targetNode = document.getElementById(this.entry.id);
 				if (!entryObj.isArch) {
-					targetNode.classList.add('archived');
+					targetNode.classList.add('remove');
 				} else {
-					targetNode.classList.replace('archived', 'highlight');
+					targetNode.classList.replace('archived', 'restore');
 				}
 				setTimeout(() => targetNode.style.opacity = 0, 100);
 				setTimeout(() => targetNode.remove(), 300);
@@ -90,7 +100,7 @@ function main() {
 			this.editBtn.className = 'entry_edit_btn';
 			this.editBtn.title = 'Edit this entry';
 			this.editBtn.addEventListener('click', (event) => {
-				openEntryForm(event);
+				openEntryForm(event, this.entry.id);
 				const titleField = document.getElementById('entry_form_title');
 				const contentField = document.getElementById('entry_form_content');
 				const content = document.getElementById(this.entry.id).querySelector('.entry_content').innerText;
@@ -99,7 +109,7 @@ function main() {
 				contentField.focus();
 			});
 
-			// build highlight button
+			// build accent button
 
 			this.starBtn = document.createElement('div');
 			this.starBtn.id = `entry_star_btn_${entryObj.id}`;
@@ -142,71 +152,156 @@ function main() {
 		}
 	}
 
-	// State change functions
+	class jrnlEntryForm {
+		constructor(event, entryId) {
 
-	const stateChange = {
-		bgBlur: {
-			on: () => document.getElementById('static_elements').classList.add('blur'),
-			off: () => document.getElementById('static_elements').classList.remove('blur'),
-		},
-	}
+			// define the form action and product
 
-	// Keydown event listeners
+			const newEntry = !event.target.id.includes('edit')
+			const formAction = newEntry ? 'create' : 'edit';
+			const formProduct = newEntry ? 'entry' : 'changes';
 
-	const keyDownActions = {
-		'F2': event => openEntryForm(event),
-		'Escape': closeEntryForm,
-		'Enter': {
-			'ctrl': handlePublishReq,
+			// build entry form container
+
+			this.entryForm = document.createElement('div');
+			this.entryForm.id = 'entry_form_container';
+		
+			// build entry form control stack
+
+			this.contStack = document.createElement('div');
+			this.contStack.className = 'control_stack form';
+			this.contStack.id = 'control_stack form';
+
+			// build discard button
+
+			this.discardEntryBtn = document.createElement('div');
+			this.discardEntryBtn.className = 'control form';
+			this.discardEntryBtn.id = `form_discard_btn`;
+			this.discardEntryBtn.title = `Discard ${formProduct}`;
+			this.discardEntryBtn.addEventListener('click', closeEntryForm);
+		
+			// build publish button
+
+			this.publishEntryBtn = document.createElement('div');
+			this.publishEntryBtn.id = 'form_publish_btn'
+			this.publishEntryBtn.className = 'control form';
+			this.publishEntryBtn.title = `Publish ${formProduct}`;
+			switch (formAction) {
+				case 'create':
+					this.publishEntryBtn.addEventListener('click', handlePublishReq);
+					break;
+				case 'edit':
+					this.publishEntryBtn.addEventListener('click', () => handlePublishEditReq(entryId));
+			}
+
+			// build entry input section
+
+			this.inputContainer = document.createElement('div');
+			this.inputContainer.className = 'entry_input';
+		
+			// build entry title input
+
+			this.entryTitle = document.createElement('input');
+			this.entryTitle.type = 'text';
+			this.entryTitle.id = 'entry_form_title';
+			this.entryTitle.placeholder = 'Type your journal title here...';
+
+			// build entry content textarea
+
+			this.entryContent = document.createElement('textarea');
+			this.entryContent.id = 'entry_form_content';
+			this.entryContent.placeholder = 'Type your journal entry here...';
+			
+			// assemble entry form
+
+			this.inputContainer.appendChild(this.entryTitle);
+			this.inputContainer.appendChild(this.entryContent);
+			this.contStack.appendChild(this.publishEntryBtn);
+			this.contStack.appendChild(this.discardEntryBtn);
+			this.entryForm.appendChild(this.contStack);
+			this.entryForm.appendChild(this.inputContainer);
+
+			// add form event listeners
+
+			this.entryForm.addEventListener('keydown', event => {
+				switch (true) {
+					case event.ctrlKey && event.key === 'Enter' && newEntry:
+						handlePublishReq();
+						break;
+					case event.ctrlKey && event.key === 'Enter' && !newEntry:
+						handlePublishEditReq(entryId);
+						break;
+					default:
+						break;
+				}
+			})
+		}
+		render() {
+			return this.entryForm;
 		}
 	}
 
-	document.addEventListener('keydown', function(event) {
-		const action = keyDownActions[event.key];
-		if (typeof action === 'function') {
-			action();
-		} else if (typeof action === 'object') {
-			if (event.ctrlKey && 'ctrl' in action) action.ctrl();
-		}
-	});
-
-	// Button event listeners
+	// static event listeners
 
 	const newEntryBtn = document.getElementById('new_entry_btn');
-	const viewArchBtn = document.getElementById('view_archive_btn');
-	const viewActiveBtn = document.getElementById('view_active_btn');
+	const showArchivedBtn = document.getElementById('view_archived_btn');
+	const showAccentedBtn = document.getElementById('view_accented_btn');
 
-	newEntryBtn.addEventListener('click', event => openEntryForm(event));
-	viewArchBtn.addEventListener('click', event => handleArchiveStatus(event));
-	viewActiveBtn.addEventListener('click', event => handleArchiveStatus(event));
+	newEntryBtn.addEventListener('click', event => {
+		openEntryForm(event);
+		const titleField = document.getElementById('entry_form_title');
+		titleField.focus();
+	});
+	showArchivedBtn.addEventListener('click', event => handleJrnlFilter(event));
+	showAccentedBtn.addEventListener('click', event => handleJrnlFilter(event));
 
-	// Administrative functions
+	// administrative functions
 
-	function handleArchiveStatus(event) {
-		const showArchive = event.target.id.includes('archive');
+	function handleJrnlFilter(event) {
+		const control = event.target;
+		const controlStack = event.target.parentNode;
 		const entryStack = document.getElementById('entry_stack');
 		entryStack.innerHTML = '';
-		loadEntries(showArchive);
-		switch (showArchive) {
-			case true:
-				newEntryBtn.style.display = 'none';
-				viewArchBtn.style.display = 'none';
-				viewActiveBtn.style.display = 'flex';
-				break;
-			default:
-				newEntryBtn.style.display = 'flex';
-				viewArchBtn.style.display = 'flex';
-				viewActiveBtn.style.display = 'none';
-				break;
+		loadEntries(chooseFilter(control));
+		updateButtonStatus(controlStack, control);
+	}
+
+	function chooseFilter(element) {
+		switch (true) {
+		case element.classList.contains('active'):
+			return viewMode.active;
+			break;
+		case element.id.includes('archive'):
+			return viewMode.archive;
+			break;
+		case element.id.includes('accent'):
+			return viewMode.accent;
+			break;
 		}
 	}
 
-	function openEntryForm(event) {
+	function updateButtonStatus(parentElement, targetElement) {
+		if (targetElement.classList.contains('active')) {
+			targetElement.classList.remove('active');
+		} else {
+			for (const child of parentElement.children) {
+				if (child.classList.contains('active')) child.classList.remove('active');
+				targetElement.classList.add('active');
+			}
+		}
+	}
+
+	function openEntryForm(event, entryId = null) {
 		document.getElementById('page_header').classList.add('blur');
 		document.getElementById('page_content').classList.add('blur');
-		const formCont = buildEntryForm(event);
-		document.body.appendChild(formCont);
-		setTimeout(() => formCont.style.opacity = 1, 50);
+		const entryForm = new jrnlEntryForm(event, entryId).render();
+		document.body.appendChild(entryForm);
+		document.addEventListener('keydown', event => handleEscKey(event));
+		setTimeout(() => entryForm.style.opacity = 1, 50);
+	}
+
+	function handleEscKey(event) {
+		if (event.key == 'Escape') closeEntryForm();
 	}
 
 	function closeEntryForm() {
@@ -214,6 +309,7 @@ function main() {
 		formCont.style.opacity = 0;
 		document.getElementById('page_header').classList.remove('blur');
 		document.getElementById('page_content').classList.remove('blur');
+		document.removeEventListener('keydown', event => handleEscKey(event));
 		setTimeout(() => formCont.remove(), 500);
 	}
 
@@ -239,13 +335,22 @@ function main() {
 				setTimeout(hideErrorCallout, 3500);
 				break;
 			case false:
-				pushEntry(buildEntryObj(entryTitle, entryBody));
+				pushEntry(new jrnlObj(entryTitle, formatEntryContent(entryBody)));
 				closeEntryForm();
 		}
 	}
 
-	function buildEntryObj(title, content) {
-		return new jrnlObj(title, formatEntryContent(content));
+	function handlePublishEditReq(entryId) {
+		const editTitle = document.getElementById('entry_form_title').value;
+		const editContent = formatEntryContent(document.getElementById('entry_form_content').value);
+		const updArr = [['title', editTitle], ['content', editContent]];
+		pushUpdate(new updateObj(entryId, updArr));
+		const entry = document.getElementById(entryId);
+		entry.querySelector('.entry_title').textContent = editTitle;
+		entry.querySelector('.entry_content').innerHTML = editContent;
+		closeEntryForm();
+		entry.classList.add('highlight')
+		setTimeout(() => entry.classList.remove('highlight'), 200);
 	}
 
 	function formatEntryContent(content) {
@@ -287,94 +392,7 @@ function main() {
 		elementArr.forEach(element => element.classList.add(className))
 	}
 
-	// Builder-Renderer functions
-
-	function buildEntryForm(event) {
-
-		const editMode = event.target.id.includes('edit');
-		const formAction = editMode ? 'Edits' : 'Entry';
-
-		console.log(`Edit mode is: ${editMode}`)
-
-		console.log(editMode);
-		console.log(formAction);
-
-		// build entry form container
-
-		const entryForm = document.createElement('div');
-		entryForm.id = 'entry_form_container';
-	
-		// build entry form control stack
-
-		const contStack = document.createElement('div');
-		contStack.className = 'control_stack form';
-		contStack.id = 'control_stack form';
-
-		// build discard button
-
-		const discardEntryBtn = document.createElement('div');
-		discardEntryBtn.className = 'control form';
-		discardEntryBtn.id = `discard_${formAction.toLowerCase()}_btn`;
-		discardEntryBtn.textContent = `Discard ${formAction}`;
-		discardEntryBtn.addEventListener('click', closeEntryForm);
-	
-		// build publish button
-
-		const publishEntryBtn = document.createElement('div');
-		publishEntryBtn.className = 'control form';
-		publishEntryBtn.id = `publish_${formAction.toLowerCase()}_btn`;
-		publishEntryBtn.textContent = `Publish ${formAction}`;
-		switch (editMode) {
-			case false:
-				console.log('PUBLISH!');
-				publishEntryBtn.addEventListener('click', handlePublishReq);
-				break;
-			case true:
-				console.log('You have chosen wisely')
-				publishEntryBtn.addEventListener('click', () => {
-					const editTitle = document.getElementById('entry_form_title').value;
-					const editContent = formatEntryContent(document.getElementById('entry_form_content').value);
-					const updArr = [['title', editTitle], ['content', editContent]];
-					const entry = event.target.parentElement.parentElement.parentElement;
-					entry.querySelector('.entry_title').textContent = editTitle;
-					entry.querySelector('.entry_content').innerHTML = editContent;
-					pushUpdate(new updateObj(entry.id, updArr));
-					closeEntryForm();
-					entry.classList.add('highlight')
-					setTimeout(() => entry.classList.remove('highlight'), 200);
-				});
-		}
-	
-		// build entry input section
-
-		const inputArea = document.createElement('div');
-		inputArea.className = 'entry_input';
-	
-		// build entry title input
-
-		const entryTitle = document.createElement('input');
-		entryTitle.type = 'text';
-		entryTitle.id = 'entry_form_title';
-		entryTitle.placeholder = 'Type your journal title here...';
-		entryTitle.autofocus = true;
-
-		// build entry content textarea
-
-		const entryContent = document.createElement('textarea');
-		entryContent.id = 'entry_form_content';
-		entryContent.placeholder = 'Type your journal entry here...';
-		
-		// assemble entry form
-
-		inputArea.appendChild(entryTitle);
-		inputArea.appendChild(entryContent);
-		contStack.appendChild(publishEntryBtn);
-		contStack.appendChild(discardEntryBtn);
-		entryForm.appendChild(contStack);
-		entryForm.appendChild(inputArea);
-	
-		return entryForm;
-	}
+	// builder-renderer functions
 
 	function buildErrorCallout() {
 		
@@ -398,21 +416,21 @@ function main() {
 	}
 
 	function renderEntry(entryObj) {
-		const entry = new jrnlEntry(entryObj);
+		const entry = new jrnlEntry(entryObj).render();
 		const entryStack = document.getElementById('entry_stack');
-		entryStack.insertBefore(entry.render(), entryStack.firstChild);
+		entryStack.insertBefore(entry, entryStack.firstChild);
 	}
 
 	// Server interaction functions
 
-	function loadEntries(archStatus = false) {
-		fetch('http://localhost:3000/journalEntries')
+	function loadEntries(filterFunction = viewMode.active) {
+		fetch(jrnlDataPath)
 		.then(res => res.json())
-		.then(jrnlData => jrnlData.filter(entry => archStatus === entry.isArch).forEach(entry => renderEntry(entry)))
+		.then(jrnlData => jrnlData.filter(filterFunction).forEach(entry => renderEntry(entry)))
 	}
 
 	function pushEntry(entryObj) {
-		fetch('http://localhost:3000/journalEntries', {
+		fetch(jrnlDataPath, {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
@@ -425,7 +443,7 @@ function main() {
 	}
 
 	function pushUpdate(updateObj) {
-		fetch(`http://localhost:3000/journalEntries/${updateObj.id}`, {
+		fetch(`${jrnlDataPath}/${updateObj.id}`, {
 			method: 'PATCH',
 			headers: {
 				'Content-Type': 'application/json',
